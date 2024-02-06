f6a3bcda4c665f40cffe1b98d8e6247dfdd71396
whisperliang <1851438730@qq.com>
2024-02-06 14:07:27 +0800

    [实验] lcnz_show.py: 各层各通道非零率显示成功

--- experiments/lcnz_show.py
+++ experiments/lcnz_show.py
@@ -5,10 +5,8 @@ import pickle
 
 from matplotlib import pyplot as plt
 
-from core.raw_dnn import RawDNN
-from dnn_models.chain import prepare_alexnet, prepare_vgg16
-from dnn_models.googlenet import prepare_googlenet
-from dnn_models.resnet import prepare_resnet50
+from core.dag_dnn import DagDNN
+from dnn_models.any_dnn_split import prepare_alexnet, prepare_vgg16, prepare_resnet50, prepare_googlenet
 
 
 plt.rcParams['font.sans-serif']=['SimHei']  # 用来正常显示中文标签
@@ -16,20 +14,21 @@ plt.rcParams['axes.unicode_minus']=False  # 用来正常显示负号
 lg = {'size': 16}
 
 if __name__ == '__main__':
-    CNN_NAME = 'ax'
-    VIDEO_NAME = 'road'
+    CNN_NAME = 'VGG'
+    VIDEO_NAME = 'parking'
     RESOLUTION = '480x720'  # 数据集的分辨率
     NFRAME_TOTAL = 400  # 数据集中的帧数
 
     TARGET_FRAME = 1  # 展示 TARGET_FRAME 和 TARGET_FRAME-1 的输出数据差值
-    DIF = False  # 差值数据还是原始数据
+    DIF = True  # 差值数据还是原始数据
 
-    cnn_loaders = {'ax': prepare_alexnet,
-                   'vg16': prepare_vgg16,
-                   'gn': prepare_googlenet,
-                   'rs50': prepare_resnet50}
-    raw_dnn = RawDNN(cnn_loaders[CNN_NAME]())
-    r_layers = raw_dnn.layers
+    cnn_loaders = {'AlexNet': prepare_alexnet,
+                   'VGG': prepare_vgg16,
+                   'ResNet': prepare_resnet50,
+                   'GoogLeNet': prepare_googlenet
+                   }
+    dag_dnn = DagDNN(cnn_loaders[CNN_NAME]())
+    r_layers = dag_dnn.layers
     data_name = f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}."
     data_name += ('' if DIF else 'o_') + 'lfcnz'
     with open(data_name, 'rb') as f:
@@ -38,8 +37,8 @@ if __name__ == '__main__':
     for l in range(len(r_layers)):
         nchan = len(lcnz[l])
         plt.scatter([l]*nchan, lcnz[l], s=5)
-    plt.gca().set_xlabel('CNN层号', fontproperties=lg)
-    plt.gca().set_ylabel('各通道的非零率', fontproperties=lg)
+    plt.gca().set_xlabel('CNN Layer index', fontproperties=lg)
+    plt.gca().set_ylabel('Nonzero-rate', fontproperties=lg)
     plt.tick_params(labelsize=13)
     plt.tight_layout()
     plt.show()
