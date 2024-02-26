c2d6da59f6bbed8405a2d5081b098eb4884168db
whisperliang <1851438730@qq.com>
2024-02-26 09:55:48 +0800

    从文件夹读取数据，显示w0到w1阶段的中间特征传输时间

--- experiments/transmission_estimation.py
+++ experiments/transmission_estimation.py
@@ -1,20 +1,12 @@
-"""读取一个LFCNZ文件，显示各层输出数据的数据量曲线：原始数据(不压缩/压缩)、差值压缩数据(实际值/预测值)
-一个子图展示一个帧，横轴为层号，纵轴为数据量（单位MB）
+"""读取一个模型流水线执行的记录文件，显示w0到w1的中间特征传输时间
+一个子图展示一个模型，横轴为帧号，纵轴为传输时间（单位S）
 """
-import pickle
-from typing import List
-
+import os
 from matplotlib import pyplot as plt
-import numpy as np
 from matplotlib.ticker import MaxNLocator
-
-from core.predictor import Predictor
-from core.raw_dnn import RawDNN
-from dnn_models.chain import prepare_alexnet, prepare_vgg16
-from dnn_models.googlenet import prepare_googlenet
-from dnn_models.resnet import prepare_resnet50
-from master.scheduler import SizedNode, Scheduler
-from schedulers.my_scheduler import MyScheduler
+from datetime import datetime
+from dataclasses import dataclass
+from typing import List, Dict, Tuple, Optional, TextIO, Union
 
 plt.rcParams['font.sans-serif']=['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False  # 用来正常显示负号
@@ -23,132 +15,256 @@ plt.rc('font',family='Times New Roman')
 from matplotlib.ticker import MaxNLocator
 import matplotlib
 matplotlib.rc('pdf', fonttype=42)
-def predict_show(s_dag: List[SizedNode], predictors: List[Predictor], lfcnz: List[List[List[float]]]):
-    SUB_NROW, SUB_NCOL = 3, 2
-    nframe = len(lfcnz[0])
-    nlayer = len(lfcnz)
-    artery = MyScheduler.get_artery(s_dag)
-    candidates = [at.id for at in artery if len(at.ancients) == 1]  # MyScheduler中的候选主干节点
-    show_cads = False
-    if nlayer != len(artery):  # 主干节点数!=总层数，说明有分叉
-        show_cads = True
-        print(f"DAG的主干节点: {candidates}")
-    org_ucps = np.array([snd.out_size[0] * snd.out_size[1] * snd.out_size[2] for snd in s_dag])*4/1024/1024  # MB
-    accuracy, reduction = [], []  # 各帧预测的平均精度, 各帧压缩减少的数据比例均值
-    reduction_mx = 0  # 各帧中最多可以减少的数据传输量比例
-    for f in range(nframe):
-        if f//(SUB_NROW*SUB_NCOL) > 0 and f%(SUB_NROW*SUB_NCOL) == 0:
-            print(f"Frame[{f-SUB_NROW*SUB_NCOL}:{f}] 预测精度均值= {np.average(np.array(accuracy))*100}% ,"
-                  f"压缩减少的数据量均值= {np.average(np.array(reduction))*100}% ,"
-                  f"单层数据最多可以减少传输的数据量比例= {reduction_mx*100}%")
 
+@dataclass
+class Event:
+    ifr_id: int  # IFR号
+    ifr_fin: bool  # 此IFR是否已完成
+    act: str  # 事件名称
+    timestamp: datetime  # 时间戳
+    is_start: bool  # start还是finish
 
-        # print(f"Frame{f}: 输入数据cnz={lfcnz[0][f]}")
-        lcnz_pred = Scheduler.predict_dag(lfcnz[0][f], s_dag, predictors)
-        dif_pred = np.array(Scheduler.lcnz2lsz(lcnz_pred, s_dag))*4/1024/1024  # 预测值, MB
-        lcnz_gt = [lfcnz[l][f] for l in range(nlayer)]
-        dif_gt = np.array(Scheduler.lcnz2lsz(lcnz_gt, s_dag))*4/1024/1024  # 实际值, MB
+def read_events(tcfile: Union[str, TextIO], ifr_num: int = -1) -> List[List[Event]]:
+    """从某个设备生成的tc文件中读取事件
+    :param tcfile tc文件名，或者file-like对象
+    :param ifr_num 所有事件中的IFR数量(id从0计数到ifr_num-1)。若未知可不填
+    :return i_evts i_evts[i]对应id=i的IFR所有事件，事件按照时间顺序排序
+    """
+    events = []
+    ifr_cnt = 0
+    if isinstance(tcfile, str):
+        tcfile = open(tcfile, 'r')
+    with tcfile:
+        for line in tcfile:
+            timestamp, is_start, act, ifr = line[:-1].split(' ')
+            timestamp = datetime.fromisoformat(timestamp)
+            is_start = (is_start == 'start')
+            ifr_id = int(ifr[3:].replace('-finished', ''))
+            ifr_fin = ('-finished' in ifr)
+            ifr_cnt = max(ifr_cnt, ifr_id+1)
+            events.append(Event(ifr_id, ifr_fin, act, timestamp, is_start))
+    i_evts = [[] for _ in range(max(ifr_cnt, ifr_num))]
+    for event in events:
+        i_evts[event.ifr_id].append(event)
+    return i_evts
 
-        acr = np.average(1 - np.abs(dif_pred-dif_gt)/dif_gt)  # 各层精度的均值
-        rdc = np.sum(org_ucps-dif_gt)/np.sum(org_ucps)  # 当前帧总共可以减少的数据传输量比例
-        rdc_mx = np.max(1 - dif_gt/org_ucps)  # 单层上最多减少的数据传输量比例
-        print(f"Frame{f}: 预测精度均值={acr*100}%, 当前帧共可减少{rdc*100}%的数据传输, "
-              f"单层数据最多可减少{rdc_mx*100}%的数据传输")
-        accuracy.append(acr)
-        reduction.append(rdc)
-        reduction_mx = max(reduction_mx, rdc_mx)
-        fig, ax = plt.subplots(figsize=(5, 4))
-        plt.tick_params(labelsize=16)
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
+@dataclass
+class Stage:
+    act: str  # 事件名称
+    thread: str  # 此阶段所属的设备线程：m->, ->w0, w0, w0->, ->w1, w1, w1->, ...
+    start: datetime  # 起始时间
+    finish: datetime  # 结束时间
 
-        x=[i for i in range(len(dif_pred))]
-        plt.plot(x,org_ucps, '--',linewidth=2,color='magenta',label='No Sparse Encoding',zorder=100) #原始数据
-        plt.plot(x,dif_gt, '-',color='darkorange',linewidth=2,label='Using Sparse Encoding',zorder=100) #差值数据实际值
-        #plt.plot(x, org_ucps, '-', linewidth=2, color='darkorange', label='No Sparse Encoding', zorder=100)  # 原始数据
-        #plt.plot(x, dif_gt, '--', color='magenta', linewidth=2, label='Using Sparse Encoding', zorder=100)  # 差值数据实际值
-        #plt.plot(x,dif_pred, '--',  label='差值数据预测值')#差值数据预测值
-        plt.fill_between(x, org_ucps, dif_gt, color='b', hatch = '//',alpha=0.5, label='Volume Reduction', zorder=112)
-        plt.xlabel('Index of CNN Layer',fontsize=18)
-        plt.ylabel('Data Volume (MB)',fontsize=18)
-        plt.legend(bbox_to_anchor=(0.25, 0.7),fontsize=16)
-        #plt.legend(loc='best',fontsize=16)
+    def __repr__(self):
+        return f"Stage('{self.trans_thread(self.thread)}':{self.act}," \
+               f"s='{self.start.isoformat(' ', timespec='milliseconds')}'," \
+               f"f='{self.finish.isoformat(' ', timespec='milliseconds')}')"
 
-        plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-        plt.grid(axis="y", linestyle='-.', zorder=0)
+    @staticmethod
+    def trans_thread(thread: str):
+        return thread.replace('$', '').replace(r'\rightarrow', '->').replace('_', '').replace(' ', '')
 
-        plt.show()
+@dataclass
+class IFRRecord:
+    ifr_id: int
+    start: datetime
+    finish: datetime
+    stages: List[Stage]
+
+    def __str__(self):
+        return f"IFRRecord(ifr={self.ifr_id}, start='{self.start.isoformat(' ', timespec='milliseconds')}', " \
+               f"finish='{self.finish.isoformat(' ', timespec='milliseconds')}', " \
+               f"stages={self.stages})"
+
+def events2records(mi_evts: List[List[Event]], w_i_evts: List[List[List[Event]]],
+                   act2trd: Dict[Tuple[str, str], str]) -> List[IFRRecord]:
+    ircds = []
+    for ifr_id in range(len(mi_evts)):
+        m_evts = mi_evts[ifr_id]  # Master中当前IFR的所有事件
+        # 从Master的事件中找到当前IFR的开始结束时间，并删除该事件
+        se, fe = -1, -1
+        for e, evt in enumerate(m_evts):
+            if evt.act == 'process':
+                if evt.is_start:
+                    assert se < 0
+                    se = e
+                else:
+                    assert fe < 0
+                    fe = e
+        start, finish = m_evts[se].timestamp, m_evts[fe].timestamp
+        m_evts.pop(fe)  # 这里要先删除后面的finish事件，这样se的索引不会变
+        m_evts.pop(se)
+        # 根据事件，生成Stage
+        ircd = IFRRecord(ifr_id, start, finish, [])
+        d_evts = [m_evts] + [(i_evts[ifr_id] if i_evts else []) for i_evts in w_i_evts]  # 设备->当前IFR的所有事件
+        d_name = ['$m$'] + [f'$w_{w}$' for w in range(len(w_i_evts))]  # d->设备名
+        tr_sevt: Optional[Event] = None  # 最近一个传输start事件
+        tr_sd: int = -1  # 最近一个传输start事件对应的设备
+        for d, evts in enumerate(d_evts):  # 遍历各设备
+            if d == 0:  # Master至少有一个事件，且第一个事件应该是start
+                assert len(evts) > 0 and evts[0].is_start
+                e = 0
+            else:
+                if len(evts) == 0:  # Worker上一个事件也没有，直接跳过
+                    continue
+                # 如果Worker上有事件，第一个事件应该是传输完成事件
+                assert evts[0].act == 'transmit' and not evts[0].is_start
+                s_evt, f_evt = tr_sevt, evts[0]
+                ircd.stages.append(Stage(s_evt.act, act2trd[d_name[tr_sd], s_evt.act],
+                                         s_evt.timestamp, f_evt.timestamp))
+                e = 1
+            while e+1 < len(evts):
+                s_evt, f_evt = evts[e], evts[e+1]
+                ircd.stages.append(Stage(s_evt.act, act2trd[d_name[d], s_evt.act], s_evt.timestamp, f_evt.timestamp))
+                e += 2
+            assert e+1 == len(evts)  # len(evts)-1应该是传输开始事件
+            assert evts[-1].act == 'transmit' and evts[-1].is_start
+            tr_sevt = evts[-1]
+            tr_sd = d
+        # 最后一个传输事件的finish时间为此IFR的finish时间，即Master收到的时间
+        ircd.stages.append(Stage(tr_sevt.act, act2trd[d_name[tr_sd], tr_sevt.act], tr_sevt.timestamp, ircd.finish))
+        ircds.append(ircd)
+    return ircds
+
+def predict_show(v1: List[float], v2: List[float]):
+
+    x=[i for i in range(len(v1))]
+    plt.plot(x,v1, '--',linewidth=2,color='magenta',label='Pao-Ding LBS',zorder=100) #原始数据
+    plt.plot(x,v2, '-',color='darkorange',linewidth=2,label='Pao-Ding DAs',zorder=100) #差值数据实际值
+    plt.fill_between(x, v1, v2, color='b', hatch = '//',alpha=0.5, label='Transmit Time Reduction', zorder=112)
+    plt.xlabel('Index of Frame',fontsize=18)
+    plt.ylabel('Transmit Time (s)',fontsize=18)
+    # plt.legend(bbox_to_anchor=(0.25, 0.7),fontsize=16)
+    plt.legend(loc='best',fontsize=16)
+
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.grid(axis="y", linestyle='-.', zorder=0)
+
+    # plt.show()
+    plt.savefig(f'trans_{dnn_name}.png', bbox_inches='tight', pad_inches=0)
+
+def read_from_folder(folder_name: str) -> Tuple[List[List[Event]], List[List[List[Event]]]]:
+    """从指定的文件夹中读取mi_evts, w_i_evts"""
+    files = os.listdir(folder_name)
+    # 找到master.tc文件
+    master_file = [file for file in files if file == 'master.tc'][0]
+    # 从master.tc读取mi_evts
+    with open(os.path.join(folder_name, master_file), 'r') as m_tcfile:
+        mi_evts = read_events(m_tcfile)
+    
+    w_i_evts = []
+    # 找到worker*.tc文件并逐个读取
+    for file in files:
+        if file.startswith('worker') and file.endswith('.tc'):
+            with open(os.path.join(folder_name, file), 'r') as w_tcfile:
+                w_i_evts.append(read_events(w_tcfile, len(mi_evts)))
+    
+    return mi_evts, w_i_evts
+
+def show_ifr_transtime(ifr_records1: List[IFRRecord], ifr_records2: List[IFRRecord]):
+    """trds为各设备的线程，按照执行顺序排列"""
+    itg_trans = []
+    # all_start = ifr_records1[0].start  # 最开始的时间
+    for ifr_rcd in ifr_records1:
+        l=len(ifr_rcd.stages)
+        #for stage in ifr_rcd.stages:
+        for i in range(l):
+            stage=ifr_rcd.stages[i]
+            print(f'records1: {stage}')
+            stage_time = (stage.finish - stage.start).total_seconds()
+            print(f'{stage_time}')
+            # 记录w0传输时间
+            if stage.thread == '$w_0\\rightarrow$' and stage.act == 'transmit':
+                itg_trans.append(stage_time)
+
+    das_trans = []
+    # all_start = ifr_records2[0].start  # 最开始的时间
+    for ifr_rcd in ifr_records2:
+        l=len(ifr_rcd.stages)
+        #for stage in ifr_rcd.stages:
+        for i in range(l):
+            stage=ifr_rcd.stages[i]
+            print(f'records2: {stage}')
+            stage_time = (stage.finish - stage.start).total_seconds()
+            print(f'{stage_time}')
+            # 记录w0传输时间
+            if stage.thread == '$w_0\\rightarrow$' and stage.act == 'transmit':
+                das_trans.append(stage_time)
+    av_itg = sum(itg_trans)/len(itg_trans)
+    av_das = sum(das_trans)/len(das_trans)
+    down_rate = (av_itg-av_das)/av_itg
+    print(f'传输时间下降{down_rate}')
+    predict_show(itg_trans, das_trans)
 
 
 if __name__ == '__main__':
-    CNN_NAME = 'ax'
-    VIDEO_NAME = 'road'
-    RESOLUTION = '480x720'  # 数据集的分辨率
-    NFRAME_TOTAL = 400  # 数据集中的帧数
-    NFRAME_SHOW = 7  # 展示数据集中的多少帧
+    # 3种模式：
+    #   l: local模式，从 LOCAL_DIR 指定的目录下寻找tc文件，根据目录下的文件名判断worker数
+    #   r: remote模式，从 REMOTE_CFG 获取远程服务器配置和worker数，下载远程tc文件
+    #   z: zip模式，从 TCZIP 指定的zip压缩包中读取tc文件，根据压缩包中的文件名判断worker数
+    MODE = 'z'
+    XLIM = None  # 横轴的最大时间, None为matplotlib自动决定, 非None时最小时间也会设置为0
+    LOCAL_DIR = 'lbc2'  # l模式下, 本地目录路径
+    REMOTE_CFG = 'device.yml'  # 远程服务器的配置文件
+    dnn_name = 'regnet'
 
-    cnn_loaders = {'ax': prepare_alexnet,
-                   'vg16': prepare_vgg16,
-                   'gn': prepare_googlenet,
-                   'rs50': prepare_resnet50}
-    raw_dnn = RawDNN(cnn_loaders[CNN_NAME]())
-    g_r_layers = raw_dnn.layers
-    with open(f".cache/{CNN_NAME}.{RESOLUTION}.sz", 'rb') as file:
-        g_s_dag = pickle.load(file)
-    with open(f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}.pred", 'rb') as file:
-        g_predictors = pickle.load(file)
-    with open(f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}.lfcnz", 'rb') as file:
-        g_lfcnz = pickle.load(file)
-    g_lfcnz = [fcnz[:NFRAME_SHOW] for fcnz in g_lfcnz]
-    predict_show(g_s_dag, g_predictors, g_lfcnz)
+    # TCZIP = './Pipeline_Execution_result/itg_alexnet.zip'  # 从zip文件中读取tc文件
+    # g_mi_evts, g_w_i_evts = read_from_zip(TCZIP)
 
-    CNN_NAME = 'vg16'
-    cnn_loaders = {'ax': prepare_alexnet,
-                   'vg16': prepare_vgg16,
-                   'gn': prepare_googlenet,
-                   'rs50': prepare_resnet50}
-    raw_dnn = RawDNN(cnn_loaders[CNN_NAME]())
-    g_r_layers = raw_dnn.layers
-    with open(f".cache/{CNN_NAME}.{RESOLUTION}.sz", 'rb') as file:
-        g_s_dag = pickle.load(file)
-    with open(f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}.pred", 'rb') as file:
-        g_predictors = pickle.load(file)
-    with open(f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}.lfcnz", 'rb') as file:
-        g_lfcnz = pickle.load(file)
-    g_lfcnz = [fcnz[:NFRAME_SHOW] for fcnz in g_lfcnz]
-    predict_show(g_s_dag, g_predictors, g_lfcnz)
+    TCFOLDER1 = f'./Pipeline_Execution_result/itg_{dnn_name}'  # 从文件夹中读取tc文件
+    g_mi_evts, g_w_i_evts = read_from_folder(TCFOLDER1)
 
-    CNN_NAME = 'gn'
-    cnn_loaders = {'ax': prepare_alexnet,
-                   'vg16': prepare_vgg16,
-                   'gn': prepare_googlenet,
-                   'rs50': prepare_resnet50}
-    raw_dnn = RawDNN(cnn_loaders[CNN_NAME]())
-    g_r_layers = raw_dnn.layers
-    with open(f".cache/{CNN_NAME}.{RESOLUTION}.sz", 'rb') as file:
-        g_s_dag = pickle.load(file)
-    with open(f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}.pred", 'rb') as file:
-        g_predictors = pickle.load(file)
-    with open(f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}.lfcnz", 'rb') as file:
-        g_lfcnz = pickle.load(file)
-    g_lfcnz = [fcnz[:NFRAME_SHOW] for fcnz in g_lfcnz]
-    predict_show(g_s_dag, g_predictors, g_lfcnz)
+    print(f"events read succeeded, n_worker={len(g_w_i_evts)}, n_ifr={len(g_mi_evts)}")
+
+    TRD2ACTS = {r'$m\rightarrow$': ['encode', 'transmit']}
+    #for wid in range(len(g_w_i_evts)):
+    TRD2ACTS[rf'$\rightarrow w_{0}$'] = ['decode']
+    TRD2ACTS[f'$w_{0}$'] = ['execute']
+    TRD2ACTS[rf'$w_{0}\rightarrow$'] = ['encode', 'transmit']
+    TRD2ACTS[rf'$\rightarrow w_{1}$'] = ['decode']
+    TRD2ACTS[f'$w_{1}$'] = ['execute']
+    TRD2ACTS[rf'$w_{1}\rightarrow$'] = ['encode', 'transmit']
+
+    ACT2TRD = {}  # (m, decode): 'm->', (w0, decode): '->w0'
+    for trd, acts in TRD2ACTS.items():
+        for act in acts:
+            ACT2TRD[trd.replace(r'\rightarrow', '').replace(' ', ''), act] = trd
+
+    g_ircds1 = events2records(g_mi_evts, g_w_i_evts, ACT2TRD)
+    total_transmit = 0  # 传输总耗时
+    for ircd in g_ircds1:
+        print(ircd)
+        total_transmit += sum((stg.finish - stg.start).total_seconds() for stg in ircd.stages if stg.act == 'transmit')
+    print(f"total_transmit={total_transmit}s")
 
 
-    CNN_NAME = 'rs50'
-    cnn_loaders = {'ax': prepare_alexnet,
-                   'vg16': prepare_vgg16,
-                   'gn': prepare_googlenet,
-                   'rs50': prepare_resnet50}
-    raw_dnn = RawDNN(cnn_loaders[CNN_NAME]())
-    g_r_layers = raw_dnn.layers
-    with open(f".cache/{CNN_NAME}.{RESOLUTION}.sz", 'rb') as file:
-        g_s_dag = pickle.load(file)
-    with open(f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}.pred", 'rb') as file:
-        g_predictors = pickle.load(file)
-    with open(f".cache/{CNN_NAME}.{VIDEO_NAME}.{RESOLUTION}.{NFRAME_TOTAL}.lfcnz", 'rb') as file:
-        g_lfcnz = pickle.load(file)
-    g_lfcnz = [fcnz[:NFRAME_SHOW] for fcnz in g_lfcnz]
-    predict_show(g_s_dag, g_predictors, g_lfcnz)
+
+    TCFOLDER2 = f'./Pipeline_Execution_result/paoding_{dnn_name}'  # 从文件夹中读取tc文件
+    g_mi_evts, g_w_i_evts = read_from_folder(TCFOLDER2)
+
+    print(f"events read succeeded, n_worker={len(g_w_i_evts)}, n_ifr={len(g_mi_evts)}")
+
+    TRD2ACTS = {r'$m\rightarrow$': ['encode', 'transmit']}
+    #for wid in range(len(g_w_i_evts)):
+    TRD2ACTS[rf'$\rightarrow w_{0}$'] = ['decode']
+    TRD2ACTS[f'$w_{0}$'] = ['execute']
+    TRD2ACTS[rf'$w_{0}\rightarrow$'] = ['encode', 'transmit']
+    TRD2ACTS[rf'$\rightarrow w_{1}$'] = ['decode']
+    TRD2ACTS[f'$w_{1}$'] = ['execute']
+    TRD2ACTS[rf'$w_{1}\rightarrow$'] = ['encode', 'transmit']
+
+    ACT2TRD = {}  # (m, decode): 'm->', (w0, decode): '->w0'
+    for trd, acts in TRD2ACTS.items():
+        for act in acts:
+            ACT2TRD[trd.replace(r'\rightarrow', '').replace(' ', ''), act] = trd
+
+    g_ircds2 = events2records(g_mi_evts, g_w_i_evts, ACT2TRD)
+    total_transmit = 0  # 传输总耗时
+    for ircd in g_ircds2:
+        print(ircd)
+        total_transmit += sum((stg.finish - stg.start).total_seconds() for stg in ircd.stages if stg.act == 'transmit')
+    print(f"total_transmit={total_transmit}s")
+
+    show_ifr_transtime(g_ircds1, g_ircds2)
 
 
