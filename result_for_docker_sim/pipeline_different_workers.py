22caaa5f4c645a02fe81098ae1e158e93417cfbb
whisperliang <1851438730@qq.com>
2024-07-10 17:08:13 +0800

    update for clearity

--- result_for_docker_sim/pipeline_different_workers.py
+++ result_for_docker_sim/pipeline_different_workers.py
@@ -8,9 +8,10 @@ average_inference = [9.26, 5.28, 4.48, 3.04, 2.68, 2.64, 2.48, 2.58]
 plt.bar(workers_num, average_inference, color='lightgreen')
 
 # 添加标题和标签
-plt.title('Average inference time over different workers')
-plt.xlabel('Workers number')
-plt.ylabel('Average inference time/s')
+plt.title('Average inference time over different workers', fontsize=16)
+plt.xlabel('Workers number', fontsize=16)
+plt.ylabel('Average inference time/s', fontsize=16)
+plt.tick_params(axis='both', labelsize=14)
 
 # 显示图形
 plt.savefig('pipeline_different_workers.png')
