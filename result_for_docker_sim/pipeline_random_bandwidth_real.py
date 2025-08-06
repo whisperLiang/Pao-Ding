b61afb03311445841af754706fe56107b2aebc15
whisperliang <1851438730@qq.com>
2025-08-06 22:01:01 +0800

    Add script to visualize dynamic bandwidths in pipeline execution
    - Implemented a new Python script to plot bandwidths for w0 and w1 along with split points.
    - Scaled bandwidth values by a factor of 8 for better visualization.
    - Created dual y-axes to represent bandwidths and layer indices.
    - Added legends and titles for clarity.
    - Saved the generated plot as a PNG file.

--- result_for_docker_sim/pipeline_random_bandwidth_real.py
+++ result_for_docker_sim/pipeline_random_bandwidth_real.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 数据
-ifr_queue = list(range(0, 20))
-w0_bandwidths = [2.27, 1.71, 1.64, 1.42, 1.71, 1.63, 1.59, 1.51, 2.22, 1.94, 1.84, 1.90, 0.11, 0.09, 1.14, 0.90, 0.84, 0.82, 0.85, 0.74] 
-w1_bandwidths = [2.23, 2.17, 1.76, 1.91, 1.73, 2.23, 1.89, 1.79, 1.72, 2.21, 1.93, 2.05, 0.06, 0.08, 1.14, 1.14, 1.14, 1.07, 1.14, 1.14]
+ifr_queue = list(range(0, 14))
+w0_bandwidths = [1.06, 0.91, 1.57, 1.39, 1.32, 1.47, 1.21, 1.52, 0.10, 0.10, 0.76, 0.93, 0.55, 0.86] 
+w1_bandwidths = [2.12, 1.43, 1.19, 1.08, 1.42, 1.36, 1.00, 1.42, 0.10, 0.11, 0.82, 0.74, 0.92, 0.76]
 # 将 w0_bandwidths 和 w1_bandwidths 的值都乘以 8
 w0_bandwidths = [x * 8 for x in w0_bandwidths]
 w1_bandwidths = [x * 8 for x in w1_bandwidths]
 
-best_split_points = [17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 40, 40, 17, 17, 17, 17, 17, 17]
+best_split_points = [5, 10, 5, 10, 5, 5, 10, 5, 40, 40, 17, 17, 17, 17]
 
 # 创建图表
 fig, ax1 = plt.subplots()
@@ -22,7 +22,7 @@ ax1.plot(ifr_queue, w1_bandwidths, color='gray', marker='x', label=r'w0$\rightar
 ax1.set_xlabel('IFR Queue Index', fontsize=14)
 ax1.set_ylabel('Bandwidth (Mbps)', fontsize=14)
 ax1.tick_params(axis='both', which='major', labelsize=12)
-ax1.set_xticks(ifr_queue)  # 设置横坐标显示 0-19 的整数
+ax1.set_xticks(ifr_queue)  # 设置横坐标显示 0-7 的整数
 ax1.set_ylim(0, 20)  # 设置 y 轴范围为 0-20
 
 # 创建第二个 y 轴
@@ -44,4 +44,4 @@ plt.title('Pipeline Execution with Dynamic Bandwidths', fontsize=16)
 
 # 显示图表
 plt.tight_layout()  # 调整布局以防止标签被裁剪
-plt.savefig('pipeline_dynamic_bandwidths.png', dpi=300)  # 保存图表为 PNG 文件
+plt.savefig('pipeline_dynamic_bandwidths_real.png', dpi=300)  # 保存图表为 PNG 文件
